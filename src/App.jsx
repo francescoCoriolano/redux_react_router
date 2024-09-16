3a9fc877f735a0b8f5a41d98280bd84006b297f5
Francesco Coriolano <francesco.coriolano@studiographene.com>
2024-09-16 12:12:18 +0100

    adjusted routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,19 @@ import Contact from "./components/Contact";
 import HomePage from "./components/HomePage";
 import "./index.css";
 import NavBar from "./components/Navbar";
+import { useRoutes } from "react-router-dom";
+
 function App() {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
-
+  const RenderRouter = () => {
+    const element = useRoutes([
+      { path: "/", element: <HomePage /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+    ]);
+    return element;
+  };
   useEffect(() => {
     dispatch(calculateTotal());
   }, [cartItems]);
@@ -19,11 +28,12 @@ function App() {
     <>
       <BrowserRouter>
         <NavBar />
-        <Routes>
+        <RenderRouter />
+        {/* <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-        </Routes>
+        </Routes> */}
       </BrowserRouter>
     </>
   );
