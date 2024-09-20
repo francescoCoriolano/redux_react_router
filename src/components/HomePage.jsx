627b1beaaa04d8218216def826d054c684a5d1b2
Francesco Coriolano <francesco.coriolano@studiographene.com>
2024-09-20 11:40:28 +0100

    added list items

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,9 +2,11 @@ import CartContainer from "./CartContainer";
 import Modal from "./Modal";
 import { useSelector } from "react-redux";
 import { ItemCard } from "./ItemCard";
+
 export const HomePage = () => {
   const { isOpen } = useSelector((state) => state.modal);
   const { itemsList } = useSelector((state) => state.cart);
+
   return (
     <div className="center">
       <h1>HomePage</h1>
